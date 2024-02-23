927aedff8f8eefe00839b45ea82e18c17f4eba59
JAlan <GeeWhyAlan@outlook.com>
2024-02-22 21:19:38 -0600

    add logout

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -45,6 +45,17 @@ def register():
     return "Created account", 201
 
 
+@bp.before_app_request
+def load_logged_in_user():
+    user_id = session.get('user_id')
+
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = get_db().execute(
+            'SELECT * FROM user WHERE id = ?', (user_id,)
+        ).fetchone()
+
 
 @bp.route('/login', methods=('POST'))
 def login():
@@ -69,14 +80,7 @@ def login():
     return 'Success', 200
 
 
-
-@bp.before_app_request
-def load_logged_in_user():
-    user_id = session.get('user_id')
-
-    if user_id is None:
-        g.user = None
-    else:
-        g.user = get_db().execute(
-            'SELECT * FROM user WHERE id = ?', (user_id,)
-        ).fetchone()
+@bp.route('/logout')
+def logout():
+    session.clear()
+    return 'Success', 200
