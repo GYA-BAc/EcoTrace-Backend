6502ceb32711c7018c66c9f523767e7850ab6946
JAlan <GeeWhyAlan@outlook.com>
2024-02-22 21:18:13 -0600

    add pre login check

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -65,5 +65,18 @@ def login():
 
     session.clear()
     session['user_id'] = user['id']
-    
+
     return 'Success', 200
+
+
+
+@bp.before_app_request
+def load_logged_in_user():
+    user_id = session.get('user_id')
+
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = get_db().execute(
+            'SELECT * FROM user WHERE id = ?', (user_id,)
+        ).fetchone()
