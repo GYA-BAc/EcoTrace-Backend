b19e28e0aac4e0fdb41debe8f97b0586b288d644
JAlan <GeeWhyAlan@outlook.com>
2024-02-22 21:29:06 -0600

    cleanup imports

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -1,5 +1,4 @@
 import functools
-
 from flask import (
     Blueprint, 
     g, 
@@ -7,9 +6,7 @@ from flask import (
     session, 
 )
 from werkzeug.security import check_password_hash, generate_password_hash
-
 from flaskr.db import get_db
-import json
 
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
