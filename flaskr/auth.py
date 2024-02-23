8f7d856262bd6639b7226cb51330be5778fc9fad
JAlan <GeeWhyAlan@outlook.com>
2024-02-22 21:23:08 -0600

    clean up imports

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -3,12 +3,8 @@ import functools
 from flask import (
     Blueprint, 
     g, 
-    redirect, 
-    render_template, 
     request, 
     session, 
-    url_for,
-    Response
 )
 from werkzeug.security import check_password_hash, generate_password_hash
 
