269880d21925a2671ec8a6b31076033be2919599
JAlan <GeeWhyAlan@outlook.com>
2024-02-23 23:57:41 -0600

    tested login

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -8,10 +8,11 @@ from flask import (
 from werkzeug.security import check_password_hash, generate_password_hash
 from flaskr.db import get_db
 
-# this works:
-# curl --json "{\"username\":\"asdf\", \"password\":\"asdf\"}" http://127.0.0.1:5000/auth/register
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
+
+# this works:
+# curl --json "{\"username\":\"asdf\", \"password\":\"asdf\"}" http://127.0.0.1:5000/auth/register
 
 @bp.route('/register', methods=['POST'])
 def register():
@@ -52,14 +53,15 @@ def load_logged_in_user():
             'SELECT * FROM user WHERE id = ?', (user_id,)
         ).fetchone()
 
-
+#curl --json "{\"username\":\"asdf\", \"password\":\"asdf\"}" http://127.0.0.1:5000/auth/login
+        
 @bp.route('/login', methods=['POST'])
 def login():
     if request.method != 'POST':
         return "Request must be POST method", 400
     
-    username = request.form['username']
-    password = request.form['password']
+    username = request.json['username']
+    password = request.json['password']
     db = get_db()
     user = db.execute(
         'SELECT * FROM user WHERE username = ?', (username,)
