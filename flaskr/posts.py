67c59bcce0f1e2afb95b16820cbba7fd5358d670
JAlan <GeeWhyAlan@outlook.com>
2024-02-26 08:03:26 -0600

    add template post routes

--- flaskr/posts.py
+++ flaskr/posts.py
@@ -1,15 +1,30 @@
 import functools
 from flask import (
     Blueprint, 
-    g, 
     request, 
-    session, 
 )
 from flaskr.db import get_db
+
+from auth import login_required
 
 
 bp = Blueprint('posts', __name__, url_prefix='/posts')
 
 @bp.route('/query', methods=['GET'])
 def query():
+    post_id = request.json['id']
+    
+
+@bp.route('/query', methods=['GET'])
+def user():
+    username = request.json['username']
+    selection = request.json['selection']
+
+
+
+
+@bp.route('/add', methods=['POST'])
+@login_required()
+def add():
     pass
+    # request.json[]
