642e6ade0ad2de55dd8950ed5063bb4072498548
JAlan <GeeWhyAlan@outlook.com>
2024-02-28 17:03:37 -0600

    add sql groups

--- flaskr/schema.sql
+++ flaskr/schema.sql
@@ -1,6 +1,9 @@
 DROP TABLE IF EXISTS user;
 DROP TABLE IF EXISTS post;
 DROP TABLE IF EXISTS images;
+DROP TABLE IF EXISTS group;
+DROP TABLE IF EXISTS userGroups;
+
 
 CREATE TABLE user (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -15,8 +18,10 @@ CREATE TABLE post (
   title TEXT NOT NULL,
   body TEXT,
   image_id INTEGER,
+  group_id INTEGER,
   FOREIGN KEY (author_id) REFERENCES user (id),
-  FOREIGN KEY (image_id) REFERENCES images (id)
+  FOREIGN KEY (image_id) REFERENCES images (id),
+  FOREIGN KEY (group_id) REFERENCES group (id)
 );
 
 CREATE TABLE images (
@@ -24,4 +29,19 @@ CREATE TABLE images (
   author_id INTEGER NOT NULL,
   data_url TEXT NOT NULL,
   FOREIGN KEY (author_id) REFERENCES user (id)
+);
+
+CREATE TABLE group (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  info TEXT NOT NULL
+);
+
+-- relational tables
+
+CREATE TABLE userGroups (
+  user_id INTEGER,
+  group_id INTEGER,
+  PRIMARY KEY (user_id, group_id),
+  FOREIGN KEY (user_id) REFERENCES user (id),
+  FOREIGN KEY (group_id) REFERENCES group (id)
 );
