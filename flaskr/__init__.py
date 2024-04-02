d1e04ea0475d6f388b5acf9034d5bc52de83344b
JAlan <GeeWhyAlan@outlook.com>
2024-04-01 19:01:59 -0500

    add cors

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,6 +1,10 @@
 import os
 
-from flask import Flask
+from flask import (
+    Flask,
+    request, 
+    make_response
+)
 
 #flask --app flaskr run --debug --host=0.0.0.0
 
@@ -45,5 +49,31 @@ def create_app(test_config=None):
 
     from . import groups
     app.register_blueprint(groups.bp)
+
+
+
+
+
+    ### CORS section
+    @app.after_request
+    def after_request_func(response):
+        origin = request.headers.get('Origin')
+        if request.method == 'OPTIONS':
+            response = make_response()
+            response.headers.add('Access-Control-Allow-Credentials', 'true')
+            response.headers.add('Access-Control-Allow-Headers', 'Content-Type')
+            response.headers.add('Access-Control-Allow-Headers', 'x-csrf-token')
+            response.headers.add('Access-Control-Allow-Methods',
+                                'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+            if origin:
+                response.headers.add('Access-Control-Allow-Origin', origin)
+        else:
+            response.headers.add('Access-Control-Allow-Credentials', 'true')
+            if origin:
+                response.headers.add('Access-Control-Allow-Origin', origin)
+
+        return response
+    
+    
 
     return app
